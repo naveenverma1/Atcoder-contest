5f194f2264ddf1bcd70b3f586d649512617b64d2
Naveen Verma <01naveenv@gmail.com>
2021-05-15 19:16:50 +0530

    （AtCoder Beginner Contest 201）

---
+++ AtCoder Beginner Contest 201/b.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+ 
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s[] = new String[n];
+        long t[] = new long[n]; 
+        long max = 0;
+        int maxnum = 0;
+        long second = 0;
+        int secondnum = 0;
+ 
+        for(int i=0; i<n; i++){
+            s[i] = scanner.next();
+            t[i] = scanner.nextLong();
+        }
+ 
+        max = t[0];
+ 
+        for(int j=1; j<n; j++){
+            if(max < t[j]){
+                second = max;
+                secondnum = maxnum;
+                max = t[j];
+                maxnum = j;
+            }else if(second < t[j]){
+                second = t[j];
+                secondnum = j;
+            }
+        }
+ 
+ 
+        String ans = s[secondnum];
+ 
+        System.out.println(ans);
+    }
+}
